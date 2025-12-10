ec9809e21999a2f1f5524f14e7e5ccf309d24167
Einsied <43851735+Einsied@users.noreply.github.com>
2025-12-10 19:52:42 +0100

    Add tests

--- domain/src/test/java/de/seuhd/campuscoffee/domain/implementation/ReviewServiceTest.java
+++ domain/src/test/java/de/seuhd/campuscoffee/domain/implementation/ReviewServiceTest.java
@@ -2,6 +2,34 @@ package de.seuhd.campuscoffee.domain.implementation;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import de.seuhd.campuscoffee.domain.exceptions.NotFoundException;
+import de.seuhd.campuscoffee.domain.exceptions.ValidationException;
+import de.seuhd.campuscoffee.domain.configuration.ApprovalConfiguration;
+import de.seuhd.campuscoffee.domain.model.objects.Pos;
+import de.seuhd.campuscoffee.domain.model.objects.Review;
+import de.seuhd.campuscoffee.domain.model.objects.User;
+import de.seuhd.campuscoffee.domain.ports.data.ReviewDataService;
+import de.seuhd.campuscoffee.domain.ports.data.UserDataService;
+import de.seuhd.campuscoffee.domain.ports.data.PosDataService;
+import de.seuhd.campuscoffee.domain.tests.TestFixtures;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 /**
  * Unit and integration tests for the operations related to reviews.
@@ -9,7 +37,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 public class ReviewServiceTest {
 
-    private final ApprovalConfiguration approvalConfiguration = getApprovalConfiguration();
+    private final ApprovalConfiguration approvalConfiguration = TestFixtures.getApprovalConfiguration();
 
     @Mock
     private ReviewDataService reviewDataService;
