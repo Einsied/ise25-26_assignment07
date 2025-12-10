f740ff60399d7fdea052427bd9c128b8659664ea
Einsied <43851735+Einsied@users.noreply.github.com>
2025-12-10 18:04:21 +0100

    Add review dto

--- api/src/main/java/de/seuhd/campuscoffee/api/dtos/ReviewDto.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/dtos/ReviewDto.java
@@ -1,15 +1,27 @@
 package de.seuhd.campuscoffee.api.dtos;
 
 import lombok.Builder;
+import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
 
 /**
  * DTO record for POS metadata.
  */
 @Builder(toBuilder = true)
 public record ReviewDto (
-    @Nullable Long id
-    // TODO: Implement ReviewDto
+    @Nullable Long id,
+    @Nullable LocalDateTime createdAt,
+    @Nullable LocalDateTime updatedAt,
+    @NonNull Long posId,
+    @NonNull Long authorId,
+    @Size(min = 1, max = 2000, message = "Review must be between 1 and 2000 characters long.")
+    @NotBlank(message = "Review cannot be empty.")
+    @NonNull String review,
+    @Nullable Boolean approved
 ) implements Dto<Long> {
     @Override
     public @Nullable Long getId() {
